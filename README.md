4f3c607eaf3d7cfb33f701da9ddd791450d741cb
Franklin Volcic <fvolcic@umich.edu>
2022-09-14 12:05:01 -0400

    Updated clocker to the first prototype.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Clocker
+
+Clocker is currently under development and is not 
+stable yet. 
+
+Clocker is a command line utility that allows a 
+user to clocker in and clocker out easily. 
+This makes for easy time tracking if you are working on 
+something where the total time is important.
+
+To use, simply run
+
+```
+clocker init # Initialize clocker application. 
+clocker in # To clock in 
+clocker out # To clock out
+
+clocker reinit # to reinitialize clocker. 
+```
+All data is saved to `~/.clocker/clocker_file.json`
--- clocker.py
+++ clocker.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+from datetime import datetime, timedelta
 import json
-import click
 import os
+import sys
+import shutil
 
-@click.command()
-@click.argument('option')
 def main(option): 
+    if option != 'init':
+        check_initialization()
     if option == "in":
         clock_in()
     elif option == "out":
@@ -17,37 +19,90 @@ def main(option):
         clock_timesheet()
     elif option == "reinit":
         clock_reinit()
-
+    elif option == "uninit":
+        clock_uninit()
 def clock_in():
-    pass
+    clock_file = ClockFile()
+    clock_file.punch_in() 
+    clock_file.save()
 
 def clock_out():
-    pass
+    clock_file = ClockFile()
+    clock_file.punch_out() 
+    clock_file.save()
+
+def clock_uninit():
+    shutil.rmtree(os.path.expanduser("~/.clocker"))
 
 def clock_init():
-    if os.path.exists("/var/lib/clocker/clocker_file.json"):
-        print("Clocker has already been initialized. Please run clocker re-init to reinitialize clocker.")
+    """Initialize clocker for the given user."""
+    if os.path.exists(os.path.expanduser("~/.clocker/clocker_file.json")):
+        print("Clocker has already been initialized. Run 'clocker reset -hard' to reinitialize clocker.")
     else:
-        os.makedirs("/var/lib/clocker")
-        with open("/var/lib/clocker/clocker_file.json") as f:
-            f.write(json.dumps({}))
+        os.makedirs(os.path.expanduser("~/.clocker"), exist_ok=True)
+        with open(os.path.expanduser("~/.clocker/clocker_file.json"), 'w') as f:
+            f.write(json.dumps({
+                "clocked_hours":[]
+            }))
 
 def clock_timesheet():
     pass
 
 def clock_reinit():
-    pass
+    shutil.rmtree(os.path.expanduser("~/.clocker"))
+    clock_init() 
+
+def check_initialization():
+    if not os.path.exists(os.path.expanduser("~/.clocker/clocker_file.json")):
+        print("Clocker has not been initialized. Run 'clocker init' before using clocker.")
+        exit(1) 
+
+def get_dt(dt_string):
+    return datetime.strptime(dt_string, "%Y-%m-%d %H:%M:%S.%f")
 
 class ClockFile:
 
     def __init__(self):
-        self.clocker_json = {} 
+        with open(os.path.expanduser("~/.clocker/clocker_file.json"), 'r') as f:
+            self.clocker_json = json.load(f) 
+
+    def _gen_punch_stamp(self, time, in_out):
+        return {"direction":in_out, "time":str(time)}
+
+    def _generate_punch(self, in_out):
+        if in_out == "in": 
+            if not len(self.clocker_json["clocked_hours"]):
+                self.clocker_json["clocked_hours"].append(self._gen_punch_stamp(str(datetime.now()), "in"))
+            elif self.clocker_json["clocked_hours"][-1]["direction"] == "in":
+                return -1, "Error: already punched in."
+            else:
+                self.clocker_json["clocked_hours"].append(self._gen_punch_stamp(str(datetime.now()), "in"))
+                print("Punched in at: " + self.clocker_json["clocked_hours"][-1]["time"])
+        if in_out == "out":
+            if not (len(self.clocker_json["clocked_hours"])):
+                return -1, "Error: not currently punched in."
+            elif self.clocker_json["clocked_hours"][-1]["direction"] == "out":
+                return -1, "Error: not currently punched in."
+            else:
+                self.clocker_json["clocked_hours"].append(self._gen_punch_stamp(str(datetime.now()), "out"))
+                time_in = get_dt(self.clocker_json["clocked_hours"][-2]["time"])
+                time_out = get_dt(self.clocker_json["clocked_hours"][-1]["time"])
+                shift_duration = time_out - time_in
+                print(f"Clocked out. Shift duration: {shift_duration}")
+                
+
 
     def punch_in(self):
-        pass
+        result = self._generate_punch("in")
+        if result:
+            print(result[1])
+            return -1
 
     def punch_out(self):
-        pass
+        result = self._generate_punch("out")
+        if result:
+            print(result[1])
+            return -1
 
     def timesheet(self, start_time, end_time):
         pass
@@ -62,7 +117,14 @@ class ClockFile:
         pass
 
     def save(self):
-        pass
+        with open(os.path.expanduser("~/.clocker/clocker_file.json"), 'w') as f:
+            f.write(json.dumps(self.clocker_json))
 
 if __name__ == "__main__":
-    main()
+    valid_args = {"in", "out", "init", "reinit"}
+    if len(sys.argv) < 2:
+        print("Error: please specify an action.")
+        exit(1)
+    if not sys.argv[1] in valid_args:
+        exit(1) 
+    main(sys.argv[1])
